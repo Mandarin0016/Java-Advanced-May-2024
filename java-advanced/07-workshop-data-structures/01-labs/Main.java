1c824df2dc06a1258374eb91dae5ac9ef6dc574b
Martin Paunov <paunovmar@gmail.com>
2024-06-03 21:59:18 +0300

    Addin solutions for data structures workshop SmartArray and CustomStack

---
+++ java-advanced/07-workshop-data-structures/01-labs/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CustomStack stack = new CustomStack();
+
+        stack.push(13);
+        stack.push(42);
+        stack.push(69);
+        stack.push(73);
+    }
+}
